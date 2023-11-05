0ae1284c73644ed85cb81d1259fb93a0f28d2eaf
Felipe Serrou Camy de Paiva <82775159+Feguri@users.noreply.github.com>
2023-11-05 17:00:06 -0500

    second commit

---
+++ index.js
@@ -0,0 +1,21 @@
+/*
+Returns an array with all the posts data.
+    Array at [0] is the Footer data
+    Array at [1] is the Our Partners data
+    Array at [2] is the Contact us data
+    Array at [3] is the Head data
+    Array at [4] is the Video data
+    Array at [5] is the Why Capy data
+    Array at [6] is the Become a mentor data
+    Array at [7] is the Quiz data
+*/
+
+fetch('https://capibility.com/headless/wp-json/wp/v2/posts')
+.then(res => res.json())
+.then(json => {
+    createLandingPage(json[3]);
+})
+
+function createLandingPage(data) {
+    console.log(data)
+}
