f6dcb2d21090ab85607a00f6aa8f15ab7048662f
Felipe Serrou Camy de Paiva <82775159+Feguri@users.noreply.github.com>
2024-03-13 15:41:48 -0400

    bg

--- js/index.js
+++ js/index.js
@@ -1,13 +1,6 @@
 /*
 Returns an array with all the posts data.
-    Array at [0] is the Footer data
-    Array at [1] is the Our Partners data
-    Array at [2] is the Contact us data
-    Array at [3] is the Head data
-    Array at [4] is the Video data
-    Array at [5] is the Why Capy data
-    Array at [6] is the Become a mentor data
-    Array at [7] is the Quiz data
+    Array at [0] is the landing page
 */
 
 const url = 'https://capibility.com/headless/wp-json/wp/v2/posts';
@@ -17,10 +10,11 @@ async function fetchFavourites(url){
     const data = await repsonse.json();
     displayData(data);
 }
-
+ 
 //call function to fetch data
 fetchFavourites(url);
 
+// a function that separates stringified HTML elements into an array.
 function separateHTMLElements(htmlString) {
   const doc = new DOMParser().parseFromString(htmlString, 'text/html');
   const elements = Array.from(doc.body.childNodes)
@@ -29,16 +23,35 @@ function separateHTMLElements(htmlString) {
   return elements;
 }
 
+// function to add a class or id or any attribute to a stringified HTML element using regex.
+function addAttribute(attributeType, attributeValue, htmlElement) {
+  const regex = /<(\w+)([^>]*)>/;
+  const match = htmlElement.match(regex);
+
+  if (match) {
+      const tagName = match[1];
+      const attributes = match[2];
+
+      const updatedElement = `<${tagName} ${attributes.trim()} ${attributeType}="${attributeValue}">`;
+      return htmlElement.replace(regex, updatedElement);
+  }
+
+  return htmlElement;
+}
+
 const title = document.getElementById('title');
 const intro = document.getElementById('intro');
+const signUp = document.getElementById('sign-up')
 function displayData(data){
   // const links = data[]
   const contentElements = data[0].content.rendered;
   const contentElementsArray = separateHTMLElements(contentElements);
 
   const introSentence = contentElementsArray[1];
+  const signUpSentence = addAttribute('class', 'sign-up', contentElementsArray[2]) ;
   const titleSentence = data[0].title.rendered;
   title.innerHTML = titleSentence;
   intro.innerHTML = introSentence;
+  signUp.innerHTML = signUpSentence;
   console.log(title, contentElementsArray, contentElements);
 }
