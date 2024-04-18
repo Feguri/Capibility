04a51b9a3b796bd6ffe906b944ad8183129c64fa
lararinaldi <130396877+lararinaldi@users.noreply.github.com>
2024-04-18 15:42:47 -0400

    arrow img change js direction

--- js/vectors.js
+++ js/vectors.js
@@ -1,16 +1,16 @@
 const vectorBtns = document.getElementsByClassName('vector-btn');
 for (let vectorBtn of vectorBtns){
-    vectorBtn.style.transform = 'rotate(180deg)';
+    vectorBtn.style.transform = 'rotate(0deg)';
     let text = document.getElementById(`p${vectorBtn.id}`)
     text.style.display ='none';
 
     vectorBtn.addEventListener('click', function() {
 
-        if(vectorBtn.style.transform === 'rotate(0deg)'){
-            vectorBtn.style.transform = 'rotate(180deg)';
+        if(vectorBtn.style.transform === 'rotate(180deg)'){
+            vectorBtn.style.transform = 'rotate(0deg)';
             text.style.display ='none';
         } else {
-            vectorBtn.style.transform = 'rotate(0deg)';
+            vectorBtn.style.transform = 'rotate(180deg)';
             text.style.display ='block';
         }
 
